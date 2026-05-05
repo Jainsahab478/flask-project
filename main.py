079a7b80ef84b6c8c0e64108dd95f19d5c56f771
Rishab Jain <nrtjjain.98911@gmail.com>
2026-05-05 17:12:08 +0000

    Added Flask MongoDB Project

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, url_for, redirect
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+from pymongo.mongo_client import MongoClient
+
+client = MongoClient(os.getenv("uri"))
+
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = os.getenv("uri")
+mongo = PyMongo(app)
+
+@app.route("/", methods=['GET'])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/Submit", methods=['POST'])
+def submit():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    gender = request.form.get("gender")
+    
+    document = {
+        "Name" : name,
+        "Email" : email,
+        "Gender" : gender
+    }
+    try:
+        mongo.db.yummy.insert_one(document)
+        return redirect(url_for("suxes"))
+    except Exception as e:
+        return f"Another Failure : {str(e)}"
+@app.route("/success")
+def suxes():
+    return "Data submitted successfully"
+
+if __name__=="__main__":
+    app.run(debug=True)
